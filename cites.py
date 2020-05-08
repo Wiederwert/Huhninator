8fd201afaac304abb4772ce6e08f37e7ed6471f0
mathuias <matthias.hupfer@telekom.de>
2020-05-08 15:33:07 +0200

    cities update

--- cites.py
+++ cites.py
@@ -1,14 +1,23 @@
 CITES = [
     {
         'id': 0,
-        'text': 'Test 0!'
+        'text': 'Reicht dann auch mit Uni :D'
     },
     {
         'id': 1,
-        'text': 'Test 1!'
+        'text': 'Also bei der Telekom ist innovatives Kaffeetrinken an der Tagesordnung :P'
     },
     {
         'id': 2,
-        'text': 'Test 2!'
+        'text': 'Jetzt schon zu viel für meine zwei Gehirnzellen'
+    },
+    {
+        'id': 3,
+        'text': 'Telekom? In Germany we call this Bundespost!'
+    },
+    {
+        'id': 4,
+        'text': 'Wann ist der zweitversuch? :D'
     }
 ]
+
